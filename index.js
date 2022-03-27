69cae0be1f39dcbeca204191633f87b38a95aaa0
Ivan Pli <plisyakov.ivan@mail.ru>
2022-03-27 21:03:22 +0700

    3

--- index.js
+++ index.js
@@ -23,9 +23,11 @@ mongoose.connect('mongodb+srv://IvanPlisyakov:actiVision12@cluster-mesto.ndo6c.m
 
 });*/
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(helmet());
+app.use(requestLogger);
+app.use(limiter);
+app.use(bodyParser.json());
 //app.use(bodyParser.json());
 app.use('/', routers);
 /*app.get('/api', async (req, res) => {
